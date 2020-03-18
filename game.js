8349328cb4940758e16af0d5efba5a9c8254a620
sunwen <sw849557369@gmail.com>
2020-03-18 18:28:05 +0800

    no message

---
+++ game.js
@@ -0,0 +1,1 @@
+"undefined"!=typeof swan&&"undefined"!=typeof swanGlobal?(require("swan-game-adapter.js"),require("libs/laya.bdmini.js")):"undefined"!=typeof wx&&(require("weapp-adapter.js"),require("libs/laya-7097c959a9.wxmini.js")),window.loadLib=require,require("index.js");
